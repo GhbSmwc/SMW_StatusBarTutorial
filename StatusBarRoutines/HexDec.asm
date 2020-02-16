b31ba0a56bfcdd0959110821cdf96776386e1aea
GhbSmwc <GhbSmwc@users.noreply.github.com>
2020-02-16 13:45:41 -0500

    Add files via upload

--- StatusBarRoutines/HexDec.asm
+++ StatusBarRoutines/HexDec.asm
@@ -181,33 +181,33 @@ incsrc "../StatusBarRoutinesDefines/Defines.asm"
 				RTL
 			endif
 
-		if !sa1 != 0
-			;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-			; unsigned 16bit / 16bit Division
-			;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-			; Arguments
-			; $00-$01 : Dividend
-			; $02-$03 : Divisor
-			; Return values
-			; $00-$01 : Quotient
-			; $02-$03 : Remainder
-			;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-			MathDiv:	REP #$20
-					ASL $00
-					LDY #$0F
-					LDA.w #$0000
-			-		ROL A
-					CMP $02
-					BCC +
-					SBC $02
-			+		ROL $00
-					DEY
-					BPL -
-					STA $02
-					SEP #$20
-					RTL
-		endif
+		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+		; unsigned 16bit / 16bit Division
+		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+		; Arguments
+		; $00-$01 : Dividend
+		; $02-$03 : Divisor
+		; Return values
+		; $00-$01 : Quotient
+		; $02-$03 : Remainder
+		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+		MathDiv:	REP #$20
+				ASL $00
+				LDY #$0F
+				LDA.w #$0000
+		-		ROL A
+				CMP $02
+				BCC +
+				SBC $02
+		+		ROL $00
+				DEY
+				BPL -
+				STA $02
+				SEP #$20
+				RTL
+
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	;32-bit hex-dec (using right-2-left division)
 	;input:
