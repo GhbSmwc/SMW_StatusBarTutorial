798f0ddb06136484ceb67a5f4b8c6218a2fefdc4
GhbSmwc <adomsyik@gmail.com>
2023-01-02 12:50:25 -0500

    update
    -RepeatedSymbols.asm: fixed some incorrect output info being in the input area and added vertical repeated icons
    
     - HexDec.asm: added stripe handlers
    
     - HowToUseTheStatusBar.html: Added test code for vertical repeating icons

--- StatusBarRoutines/HexDec.asm
+++ StatusBarRoutines/HexDec.asm
@@ -1,38 +1,41 @@
 incsrc "../StatusBarRoutinesDefines/Defines.asm"
 ;Routines list:
 ;General math routines:
-; -MathDiv
-; -MathDiv32_16
-; -MathMul16_16
+; - MathDiv
+; - MathDiv32_16
+; - MathMul16_16
 ;HexDec routines:
-; -EightBitHexDec
-; -EightBitHexDec3Digits
-; -SixteenBitHexDecDivision
-; -ThirtyTwoBitHexDecDivision
+; - EightBitHexDec
+; - EightBitHexDec3Digits
+; - SixteenBitHexDecDivision
+; - ThirtyTwoBitHexDecDivision
 ;Leading zeroes remover (leading spaces):
-; -RemoveLeadingZeroes16Bit
-; -RemoveLeadingZeroes32Bit
-; -RemoveLeadingZeroes16BitLeaveLast2
-; -RemoveLeadingZeroes16BitLeaveLast3
+; - RemoveLeadingZeroes16Bit
+; - RemoveLeadingZeroes32Bit
+; - RemoveLeadingZeroes16BitLeaveLast2
+; - RemoveLeadingZeroes16BitLeaveLast3
 ;Overworld:
-; -SixteenBitHexDecDivisionToOWB
-; -ThirtyTwoBitHexDecDivisionToOWB
+; - SixteenBitHexDecDivisionToOWB
+; - ThirtyTwoBitHexDecDivisionToOWB
 ;Leading zeroes remover (left or aligned digits):
-; -SupressLeadingZeros
-; -SupressLeadingZerosPercentageLeaveLast2
-; -SupressLeadingZerosPercentageLeaveLast3
-; -SupressLeadingZeros32Bit
-; -ConvertToRightAligned
-; -ConvertToRightAlignedFormat2
+; - SupressLeadingZeros
+; - SupressLeadingZerosPercentageLeaveLast2
+; - SupressLeadingZerosPercentageLeaveLast3
+; - SupressLeadingZeros32Bit
+; - ConvertToRightAligned
+; - ConvertToRightAlignedFormat2
 ;Aligned digit to OWB digits:
-; -ConvertAlignedDigitToOWB
+; - ConvertAlignedDigitToOWB
 ;Write to status bar or overworld border:
-; -WriteStringDigitsToHUD
-; -WriteStringDigitsToHUDFormat2
+; - WriteStringDigitsToHUD
+; - WriteStringDigitsToHUDFormat2
 ;Misc:
-; -Frames2Timer
-; -ConvertToPercentage
-; -CountingAnimation16Bit
+; - Frames2Timer
+; - ConvertToPercentage
+; - CountingAnimation16Bit
+;Stripe routines
+; - SetupStripeHeaderAndIndex
+; - FinishStripe
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;General math routines.
 ;Due to the fact that registers have limitations and such.
@@ -1144,3 +1147,155 @@ CountingAnimation16Bit:
 	.NoChange
 	SEP #$20
 	RTL
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;Easy stripe setup-er. Gets index of stripe table and sets up the header.
+;Input:
+;-$00: X position (%00XXXXXX, only bits 0-5 used, ranges from 0-63 ($00-$3F))
+;-$01: Y position (%00YYYYYY, only bits 0-5 used, ranges from 0-63 ($00-$3F))
+;-$02: What layer:
+;       $02 = Layer 1
+;       $03 = Layer 2
+;       $05 = Layer 3
+;-$03: Direction and RLE: %DR00000000
+;       D = Direction: 0 = horizontal (rightwards), 1 = vertical (downwards)
+;       R = RLE: 0 = no repeat, 1 = repeat
+;-$04 to $05 (16-bit): Number of tiles.
+;Output:
+;-X register (16-bit, XY registers are 16-bit): The index position to write stripe data
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;note to self
+; $7F837D+0,x = EHHHYXyy
+; $7F837D+1,x = yyyxxxxx
+; $7F837D+2,x = DRllllll
+; $7F837D+3,x = LLLLLLLL
+SetupStripeHeaderAndIndex:
+	.GetWhereToSafelyWriteStripe
+		REP #$30		;>16-bit AXY
+		LDA $7F837B		;\LDX $XXXXXX does not exist so we need LDA $XXXXXX : TAX to
+		TAX			;/get RAM values stored in bank $7F into X register.
+	.StartWithBlankHeaderInitally
+		LDA #$0000		;\Clear everything out first
+		STA $7F837D+0,x		;|
+		STA $7F837D+2,x		;/
+		SEP #$20
+	.Xposition
+		LDA $00			;\X bit 0-4
+		AND.b #%00011111	;|
+		ORA $7F837D+1,x		;|
+		STA $7F837D+1,x		;/
+		LDA $00			;\X bit 5
+		AND.b #%00100000	;|
+		LSR #3			;|
+		ORA $7F837D+0,x		;|
+		STA $7F837D+0,x		;/
+	.Yposition
+		LDA $01			;\Y bit 0-2
+		AND.b #%00000111	;|
+		ASL #5			;|
+		ORA $7F837D+1,x		;|
+		STA $7F837D+1,x		;/
+		LDA $01			;\Y bit 3-4
+		AND.b #%00011000	;|
+		LSR #3			;|
+		ORA $7F837D+0,x		;|
+		STA $7F837D+0,x		;/
+		LDA $01			;\Y bit 5
+		AND.b #%00100000	;|
+		LSR #2			;|
+		ORA $7F837D+0,x		;|
+		STA $7F837D+0,x		;/
+	.WhatLayer
+		LDA $02
+		AND.b #%00000111
+		ASL #4
+		ORA $7F837D+0,x
+		STA $7F837D+0,x
+	.Direction
+		LDA $03
+		AND.b #%11000000	;>Failsafe
+		ORA $7F837D+2,x
+		STA $7F837D+2,x
+	.Length
+		AND.b #%01000000
+		BEQ ..NoRLE
+		
+		..RLE
+			REP #$20
+			LDA $04			;\NumberOfBytes = (NumberOfTiles-1)*2
+			DEC A			;|
+			ASL			;|
+			SEP #$20		;/
+			BRA ..Write
+		..NoRLE
+			REP #$20
+			LDA $04			;\NumberOfBytes = (NumberOfTiles*2)-1
+			ASL			;|
+			DEC			;|
+			SEP #$20		;/
+		..Write
+			STA $7F837D+3,x		;\Write
+			XBA			;|
+			AND.b #%00111111	;|
+			ORA $7F837D+2,x		;|
+			STA $7F837D+2,x		;/
+	.Done
+		RTL
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;Finish stripe write
+;
+;This writes the terminating byte $FF and updates the length of stripe.
+;
+;Input:
+;-$03: Direction and RLE: %DR00000000. This routine only checks the RLE
+;      due to the length formula varies depending if using RLE or not.
+;-$04 to $05: Number of tiles
+;-X register (16-bit): Index length of stripe table
+;Output:
+;-$7F837B to $7F837C: New length of stripe
+;Destroyed:
+;-$00 to $02: Address of where to write the terminating byte based
+;             on the indexing and number of tiles.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+FinishStripe:
+	.UpdateLength
+		LDA $03
+		AND.b #%01000000
+		BEQ ..NoRLE
+		
+		..RLE
+			REP #$21			;REP #$21 is 8-bit A with carry cleared
+			TXA				;\Update length of stripe. 6 because 2 bytes of 1 tile plus 4 bytes of header)
+			ADC #$0006			;|
+			STA $7F837B			;/
+			SEP #$30			;>8-bit AXY
+			...WhereToWriteTerminateByte
+				LDA #$FF
+				STA $7F837D+6,x
+			RTL
+		..NoRLE
+			REP #$21			;REP #$21 is 8-bit A with carry cleared
+			LDA $04				;\4+(NumberOfTiles*2)...
+			ASL				;|
+			CLC				;|
+			ADC #$0004			;/
+			CLC				;\plus the current length
+			ADC $7F837B			;/
+			STA $7F837B			;>And that is our new length
+			SEP #$30			;>8-bit AXY
+			...WhereToWriteTerminateByte
+				LDA #$7F		;\Bank byte
+				STA $02			;/
+				REP #$20		;\4+(NumberOfTiles*2)...
+				LDA $04			;|
+				ASL			;|
+				CLC			;|>Just in case
+				ADC.w #$837D+4		;|
+				STA $00			;/
+				TXA			;\Plus index ($7F837D+(NumberOfBytesSinceHeader),x is equivilant to $7F837D + NumberOfBytesSinceHeader + X_index)
+				CLC			;|
+				ADC $00			;|
+				STA $00			;/
+				SEP #$20
+				LDA #$FF		;\Write terminate byte here.
+				STA [$00]		;/
+			RTL
--- StatusBarRoutines/RepeatedSymbols.asm
+++ StatusBarRoutines/RepeatedSymbols.asm
@@ -1,21 +1,33 @@
 incsrc "../StatusBarRoutinesDefines/Defines.asm"
 
+;Routines listed
+; - Horizontal (can be used on stripe image)
+; -- WriteRepeatedSymbols
+; -- WriteRepeatedSymbolsLeftwards
+; -- WriteRepeatedSymbolsFormat2
+; -- WriteRepeatedSymbolsLeftwardsFormat2
+; - Vertical (don't use thesse for stripe image)
+; -- WriteRepeatedSymbolsWriteVertically
+; -- WriteRepeatedSymbolsWriteVerticallyFormat2
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;Input:
 ; -$00 (1 byte): the amount filled
 ; -$01 (1 byte): the total amount (or maximum)
 ; -$02 (1 byte): Tile number for empty
 ; -$03 (1 byte): Tile number for full
-; -$04-$06 (3 bytes): The status bar address of tiles to write.
+; -$04-$06 (3 bytes): The status bar address of tiles to write (leftmost tile position,
+;  for both rightwards and leftwards).
 ; -If you have !StatusBar_UsingCustomProperties set to 1, the following will be used:
 ; --$07 (1 byte): Tile properties for empty
 ; --$08 (1 byte): Tile properties for full
 ; --$09-$0B (3 bytes): The status bar address of tile properties to write
-; --RAM_In_Addr09 to [RAM_In_Addr09 + ((ValueIn01-1) * !StatusbarFormat)] The
-;    repeated tile properties in question.
+
 ;Output: 
-; RAM_In_Addr06 to [RAM_In_Addr06 + ((ValueIn01-1) * !StatusbarFormat)] The
+; -RAM_In_Addr04 to [RAM_In_Addr04 + ((ValueIn01-1) * !StatusbarFormat)] The
 ;  repeated tiles in question.
+; -RAM_In_Addr09 to [RAM_In_Addr09 + ((ValueIn01-1) * !StatusbarFormat)] The
+;   repeated tile properties in question.
 ;Overwritten:
 ; $00-$01: will be 0 as they're being used to count
 ;  how many tiles left to write.
@@ -155,3 +167,120 @@ WriteRepeatedSymbolsLeftwardsFormat2:
 			BNE .Loop
 	.Done
 		RTL
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;Input:
+; -$00 (1 byte): the amount filled
+; -$01 (1 byte): the total amount (or maximum)
+; -$02 (1 byte): Tile number for empty
+; -$03 (1 byte): Tile number for full
+; -$04-$06 (3 bytes): The status bar address of tile numbers to write (position of the first tile
+;  that fills up when increasing).
+; -Not used if !StatusBar_UsingCustomProperties == 0:
+;  --$07 (1 byte): Tile properties for empty
+;  --$08 (1 byte): Tile properties for full
+;  --$09-$0B (3 bytes): Same as $04-$06 but for tile properties.
+; -$0C (1 byte): Direction, only use these values: $00 = upwards, $01 = downwards
+;Output:
+; -[RAM_In_Addr04]-(X*32*!StatusbarFormat) where X increases from 0 to NumberOfTiles-1
+;  for upwards, [RAM_In_Addr04]+(X*32*!StatusbarFormat) where X increases from 0 to
+;  NumberOfTiles-1 for downwards:
+;  the tiles written to the status bar
+; -Not used if !StatusBar_UsingCustomProperties == 0:
+; --[RAM_In_Addr09]-(X*32*!StatusbarFormat) where X increases from 0 to NumberOfTiles-1
+;   for upwards, [RAM_In_Addr09]+(X*32*!StatusbarFormat) where X increases from 0 to
+;   NumberOfTiles-1 for downwards: the tile properties written to status bar
+; -$04-$06 (3 bytes): The address after writing the last tile (as if writing the amount
+;  of tiles plus 1), can be used for writing a tile where the last tile is written.
+; -$09-$0B (2 bytes): Same as above.
+;
+;NOTE: this only works with status bars having a row of 32 8x8 tiles, rows are lined up vertically,
+;and each row being contiguous to each other. Else your tiles won't line up vertically.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+WriteRepeatedSymbolsWriteVertically:
+	.Loop
+		LDA $01			;\If no tiles left, done.
+		BEQ .Done		;/
+		LDA $00			;\If no full tiles, write empty.
+		BEQ ..Empty		;/
+		..Full
+			LDA $03			;\Write full tile
+			STA [$04]		;/
+			if !StatusBar_UsingCustomProperties != 0
+				LDA $08			;\Write full tile properties
+				STA [$09]		;/
+			endif
+			DEC $00			;>Decrement how much full tiles left.
+			BRA ..Next
+		..Empty
+			LDA $02			;\Write empty tile
+			STA [$04]		;/
+			if !StatusBar_UsingCustomProperties != 0
+				LDA $07			;\Write empty tile properties
+				STA [$09]		;/
+			endif
+		..Next
+			LDX $0C							;\Offset
+			REP #$20						;|
+			LDA $04							;|
+			CLC							;|
+			ADC .WriteRepeatedIconsVerticallyUpDownDisplacement,x	;|
+			STA $04							;|
+			if !StatusBar_UsingCustomProperties != 0
+				LDA $09							;|
+				CLC							;|
+				ADC .WriteRepeatedIconsVerticallyUpDownDisplacement,x	;|
+				STA $09							;|
+			endif
+			SEP #$20						;/
+			
+			DEC $01			;>Decrement how many total tiles are left.
+			BNE .Loop
+	.Done
+		RTL
+	.WriteRepeatedIconsVerticallyUpDownDisplacement
+		dw -32		;>RAM $0A = $00
+		dw 32		;>RAM $0A = $02
+WriteRepeatedSymbolsWriteVerticallyFormat2:
+	.Loop
+		LDA $01			;\If no tiles left, done.
+		BEQ .Done		;/
+		LDA $00			;\If no full tiles, write empty.
+		BEQ ..Empty		;/
+		..Full
+			LDA $03			;\Write full tile
+			STA [$04]		;/
+			if !StatusBar_UsingCustomProperties != 0
+				LDA $08			;\Write full tile properties
+				STA [$09]		;/
+			endif
+			DEC $00			;>Decrement how much full tiles left.
+			BRA ..Next
+		..Empty
+			LDA $02			;\Write empty tile
+			STA [$04]		;/
+			if !StatusBar_UsingCustomProperties != 0
+				LDA $07			;\Write empty tile properties
+				STA [$09]		;/
+			endif
+		..Next
+			LDX $0C								;\Offset
+			REP #$20							;|
+			LDA $04								;|
+			CLC								;|
+			ADC .WriteRepeatedIconsVerticallyUpDownDisplacementFormat2,x	;|
+			STA $04								;|
+			if !StatusBar_UsingCustomProperties != 0
+				LDA $09								;|
+				CLC								;|
+				ADC .WriteRepeatedIconsVerticallyUpDownDisplacementFormat2,x	;|
+				STA $09								;|
+			endif
+			SEP #$20						;/
+			
+			DEC $01			;>Decrement how many total tiles are left.
+			BNE .Loop
+	.Done
+		RTL
+	.WriteRepeatedIconsVerticallyUpDownDisplacementFormat2
+		dw -64		;>RAM $0A = $00
+		dw 64		;>RAM $0A = $02
