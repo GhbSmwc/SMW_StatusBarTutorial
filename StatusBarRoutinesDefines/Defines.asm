2a40644870e8934fac843ff5e80aa4814d38a9d1
? <GhbSmwc@users.noreply.github.com>
2025-03-09 15:24:17 -0400

    oops

--- StatusBarRoutinesDefines/Defines.asm
+++ StatusBarRoutinesDefines/Defines.asm
@@ -33,7 +33,7 @@
 	!Setting_GraphicalBar_SNESMathOnly = 0
 		;^Info follows:
 		; - Set this to 0 if your code calls the routines under the SA-1 processor.
-		;  Otherwise set it to 1 if it calls it under the SNES CPU.
+		;   Otherwise set it to 1 if it calls it under the SNES CPU.
 		;
 		; As an important note: certain emulators follows a rule that only the correct CPU can access
 		; the registers of the matching type (e.g. SA-1 registers can only be used by SA-1 CPU, not SNES)
