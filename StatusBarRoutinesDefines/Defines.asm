6df9a8e839caf91967d8b620f8e7edad8dc7acfc
? <GhbSmwc@users.noreply.github.com>
2025-03-09 15:23:49 -0400

    bulleting

--- StatusBarRoutinesDefines/Defines.asm
+++ StatusBarRoutinesDefines/Defines.asm
@@ -15,9 +15,9 @@
 	;[5 bytes]
 	;16-bit Hexdec Digit table.
 	;For these routines:
-	;-SixteenBitHexDecDivision
-	;-RemoveLeadingZeroes16Bit
-	;-SupressLeadingZeros
+	; - SixteenBitHexDecDivision
+	; - RemoveLeadingZeroes16Bit
+	; - SupressLeadingZeros
 	;This is due to the fact that the digit table
 	;position varies as the 16-bit HexDec routine
 	;uses the SNES registers for non SA-1, or
@@ -32,8 +32,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	!Setting_GraphicalBar_SNESMathOnly = 0
 		;^Info follows:
-		;-Set this to 0 if your code calls the routines under the SA-1 processor.
-		; Otherwise set it to 1 if it calls it under the SNES CPU.
+		; - Set this to 0 if your code calls the routines under the SA-1 processor.
+		;  Otherwise set it to 1 if it calls it under the SNES CPU.
 		;
 		; As an important note: certain emulators follows a rule that only the correct CPU can access
 		; the registers of the matching type (e.g. SA-1 registers can only be used by SA-1 CPU, not SNES)
@@ -62,14 +62,14 @@
 		; is the highest number of characters you would write
 		; in your entire game.
 		; For example:
-		; -If you want to display a 5-digit 16-bit number 65535,
-		;  that will be 5 bytes.
-		; -If you want to display [10000/10000], that will be
-		;  11 bytes (2 numbers up to 5 digits, plus 1 because
-		;  "/"; 5 + 5 + 1 = 11)
-		; -For 32-bit hexdec:
-		; --For displaying a left-aligned number will be !Setting_32bitHexDec_MaxNumberOfDigits
-		; --For X/Y display: (!Setting_32bitHexDec_MaxNumberOfDigits*2)+1
+		; - If you want to display a 5-digit 16-bit number 65535,
+		;   that will be 5 bytes.
+		; - If you want to display [10000/10000], that will be
+		;   11 bytes (2 numbers up to 5 digits, plus 1 because
+		;   "/"; 5 + 5 + 1 = 11)
+		; - For 32-bit hexdec:
+		; -- For displaying a left-aligned number will be !Setting_32bitHexDec_MaxNumberOfDigits
+		; -- For X/Y display: (!Setting_32bitHexDec_MaxNumberOfDigits*2)+1
 	;For 32-bit timer frame to Hours:Minutes:Seconds:Centiseconds format.
 		if !sa1 == 0
 			!Scratchram_Frames2TimeOutput = $7F8458
@@ -77,10 +77,10 @@
 			!Scratchram_Frames2TimeOutput = $40416A
 		endif
 		;^[4 bytes], the output in HH:MM:SS.CC format:
-		; !Scratchram_Frames2TimeOutput+0 = hour
-		; !Scratchram_Frames2TimeOutput+1 = minutes
-		; !Scratchram_Frames2TimeOutput+2 = seconds
-		; !Scratchram_Frames2TimeOutput+3 = centiseconds (display 00-99)
+		; - !Scratchram_Frames2TimeOutput+0 = hour
+		; - !Scratchram_Frames2TimeOutput+1 = minutes
+		; - !Scratchram_Frames2TimeOutput+2 = seconds
+		; - !Scratchram_Frames2TimeOutput+3 = centiseconds (display 00-99)
 	;For percentage converter and displays
 		if !sa1 == 0
 			!Scratchram_PercentageQuantity = $7F846D
@@ -100,10 +100,10 @@
 			!Scratchram_PercentageFixedPointPrecision = $404172
 		endif
 		;^[1 byte] Determines how many digits of display via percentage fixed point:
-		;  $00 = XXX%. Overflows if over 65535%.
-		;  $01 = XXX.X%, an integer, scaled by 1/10 (example: 503 as an integer stored is 50.3%).
-		;        Overflows if over 6553.5%
-		;  $02 = XXX.XX%. Same as above but scaled by 1/100. Overflows if over 655.35%.
+		; - $00 = XXX%. Overflows if over 65535%.
+		; - $01 = XXX.X%, an integer, scaled by 1/10 (example: 503 as an integer stored is 50.3%).
+		;   Overflows if over 6553.5%
+		; - $02 = XXX.XX%. Same as above but scaled by 1/100. Overflows if over 655.35%.
 		;
 		;Overflows, as in if you use the 16-bit hexdec. But very unlikely your hack allows
 		;percentages over 100.
