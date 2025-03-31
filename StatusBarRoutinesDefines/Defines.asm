54ec19b8981ad807ff4b45be5efee492198fd3b5
? <GhbSmwc@users.noreply.github.com>
2025-03-31 17:31:27 -0400

    ...

--- StatusBarRoutinesDefines/Defines.asm
+++ StatusBarRoutinesDefines/Defines.asm
@@ -32,11 +32,11 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	!Setting_GraphicalBar_SNESMathOnly = 0
 		;^Info follows:
-		; - Set this to 0 if your code calls the routines under the SA-1 processor.
-		;   Otherwise set it to 1 if it calls it under the SNES CPU.
+		;-Set this to 0 if your code calls the routines under the SA-1 processor on SA-1 ROM. Otherwise set
+		; it to 1 if it only calls it under the SNES CPU.
 		;
 		; As an important note: certain emulators follows a rule that only the correct CPU can access
-		; the registers of the matching type (e.g. SA-1 registers can only be used by SA-1 CPU, not SNES)
+		; the registers of the matching type (e.g. SA-1 registers can only be used by SA-1 CPU, not SNES).
 	;32-bit Number display
 		;For [Convert32bitIntegerToDecDigits]
 			!Setting_32bitHexDec_MaxNumberOfDigits = 10
